58ccf4f5af7814a3ab68e57b97ce0775ad8610a2
N.I <iworksWeb@gmail.com>
2025-01-22 23:22:43 +0900

    fix TodoApp test to use renderWithQueryClient helper and added screen debugging

--- client/src/test/components/TodoApp.test.tsx
+++ client/src/test/components/TodoApp.test.tsx
@@ -1,8 +1,7 @@
 import { describe } from "vitest";
 import TodoApp from "../../components/TodoApp";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
 const queryClient = new QueryClient();
 const renderWithQueryClient = (component: React.ReactNode) => {
   return render(
@@ -12,12 +11,7 @@ const renderWithQueryClient = (component: React.ReactNode) => {
 
 describe("TodoApp", () => {
   test("テスト", () => {
-    render(
-      <QueryClientProvider client={queryClient}>
-        renderWithQueryClient(
-        <TodoApp />
-        );
-      </QueryClientProvider>
-    );
+    renderWithQueryClient(<TodoApp />);
+    screen.debug();
   });
 });
