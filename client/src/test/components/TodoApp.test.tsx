7b5541055e29ab736b4985b75cc1523597edf24d
N.I <iworksWeb@gmail.com>
2025-01-22 23:18:30 +0900

    fix Error: No QueryClient set, use QueryClientProvider to set one

--- client/src/test/components/TodoApp.test.tsx
+++ client/src/test/components/TodoApp.test.tsx
@@ -1,9 +1,23 @@
 import { describe } from "vitest";
 import TodoApp from "../../components/TodoApp";
 import { render } from "@testing-library/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+const renderWithQueryClient = (component: React.ReactNode) => {
+  return render(
+    <QueryClientProvider client={queryClient}>{component}</QueryClientProvider>
+  );
+};
 
 describe("TodoApp", () => {
   test("テスト", () => {
-    render(<TodoApp />);
+    render(
+      <QueryClientProvider client={queryClient}>
+        renderWithQueryClient(
+        <TodoApp />
+        );
+      </QueryClientProvider>
+    );
   });
 });
