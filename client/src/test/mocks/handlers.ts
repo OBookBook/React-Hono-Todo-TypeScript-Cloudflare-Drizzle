b2cf116038712eec2ce729fbeb690c4272cdc9f6
N.I <iworksWeb@gmail.com>
2025-01-23 09:43:20 +0900

    fix TodoApp test setup: reset todos before each test and update ID handling
    - Refactored the todos array to be reset before each test, ensuring a clean state for tests.
    - Changed the ID generation for new todos to use string format for consistency with the existing data structure.
    - Improved test reliability by ensuring todos are consistently initialized for each test case.

--- client/src/test/mocks/handlers.ts
+++ client/src/test/mocks/handlers.ts
@@ -1,10 +1,15 @@
 // @Docs https://mswjs.io/docs/getting-started
 import { http, HttpResponse } from "msw";
+import { Todo } from "../../types/types";
 
-const todos = [
-  { id: 1, title: "Todo 1", status: "done", createdAt: "2025-01-01" },
-  { id: 2, title: "Todo 2", status: "done", createdAt: "2025-01-02" },
-];
+let todos: Todo[] = [];
+// テストごとにtodosをリセットする
+beforeEach(() => {
+  todos = [
+    { id: "1", title: "Todo 1", status: "done", createdAt: "2025-01-01" },
+    { id: "2", title: "Todo 2", status: "done", createdAt: "2025-01-02" },
+  ];
+});
 
 // モックハンドラーを定義 インターセプトするURLを指定
 export const handlers = [
@@ -14,7 +19,7 @@ export const handlers = [
   http.post("http://127.0.0.1:8787/todos", async (req) => {
     const todoTitle = (await req.request.json()) as { title: string };
     const newTodo = {
-      id: todos.length + 1,
+      id: String(todos.length + Number(1)),
       title: todoTitle.title,
       status: "done",
       createdAt: new Date().toISOString(),
